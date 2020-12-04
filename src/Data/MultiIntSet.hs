12df2340755b4342a52a7a57a4bacb3a7ecbb20a
Nolrai <chrisaupshaw@gmail.com>
2020-12-03 23:43:30 -0800

    removed unused LANGUAGE pragmas

--- src/Data/MultiIntSet.hs
+++ src/Data/MultiIntSet.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
